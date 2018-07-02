a6ed7dd137a7014ef65760aefa1c2d66fa3782c8
cliffhanger407 <charlie.morn@gmail.com>
2018-07-02 15:28:45 -0400

    Repairing help statements.

--- app.js
+++ app.js
@@ -250,19 +250,19 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setActivity(`For help go to https://github.com/cliffhanger407/signup-bot/blob/master/README.md`);
+  client.user.setActivity(`For help go to https://goo.gl/QLSNVU`);
 });
 
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`For help go to https://goo.gl/QLSNVU`);
 });
 
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`For help go to https://goo.gl/QLSNVU`);
 });
 
 
